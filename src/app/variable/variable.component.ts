3eb2f33dda95d414e428a23b8e7cd44a2bec5882
Maxime Duchene <duchene.maxime@outlook.fr>
2018-06-08 15:57:53 +0200

    tp 3.6 terminé

--- src/app/variable/variable.component.ts
+++ src/app/variable/variable.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Formation } from '../interpolate/interpolate.component';
 
 @Component({
   selector: 'app-variable',
@@ -6,10 +7,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./variable.component.css']
 })
 export class VariableComponent implements OnInit {
+  listFormations:Array<Formation> = [];
+
+  isDisplay: boolean = true;
 
   constructor() { }
 
   ngOnInit() {
+    this.listFormations.push(new Formation("test1", "blabla1"));
+    this.listFormations.push(new Formation("test2", "blabla2"));
+    this.listFormations.push(new Formation("test3", "blabla3"));
+  }
+
+  toHide(){
+    this.isDisplay = !this.isDisplay;
+  }
+
+  colorList(index:number){
+    if(index==0)
+      return "red";
+    if(index==this.listFormations.length-1)
+      return "green";
+  }
+
+  backgroundColorListe(index:number){
+    if(index%2==0)
+      return "grey";
   }
 
 }
