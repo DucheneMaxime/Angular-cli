c03ae8c3139bdd02513eea362cda5d022b3c39b6
Maxime Duchene <duchene.maxime@outlook.fr>
2018-06-08 14:59:37 +0200

    tp 3.5 terminé

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { FormationComponent } from './formation/formation.component';
 import { InterpolateComponent } from './interpolate/interpolate.component';
+import { VariableComponent } from './variable/variable.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FormationComponent,
-    InterpolateComponent
+    InterpolateComponent,
+    VariableComponent
   ],
   imports: [
     BrowserModule
