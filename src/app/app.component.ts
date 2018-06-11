4df6a2342cb0e2191ed1d44d3b4fff1a55cefadf
Maxime Duchene <duchene.maxime@outlook.fr>
2018-06-11 09:32:18 +0200

    tp 3.10 terminé

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Formation } from './interpolate/interpolate.component';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +8,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  listFormations:Array<Formation> = [];
+
+  constructor() { }
+
+  ngOnInit() {
+    this.listFormations.push(new Formation("test1", "blabla1"));
+    this.listFormations.push(new Formation("test2", "blabla2"));
+    this.listFormations.push(new Formation("test3", "blabla3"));
+  }
 
   handleFormationSelected(formation){
     console.log("Formation selected", formation);
